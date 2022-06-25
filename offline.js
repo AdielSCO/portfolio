c90b1f6c93cc8934b2b77ae9b073d0218c0323d0
Adiel dos Santos <93499549+AdielSCO@users.noreply.github.com>
2022-06-25 16:41:28 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1656185517,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bgum-sheet0.png",
+		"images/hellotext-sheet0.png",
+		"images/mynametext-sheet0.png",
+		"images/adieltext-sheet0.png",
+		"images/interativobt-sheet0.png",
+		"images/minimalistbt-sheet0.png",
+		"images/instagram_-sheet0.png",
+		"images/facebook-sheet0.png",
+		"images/whatsapp-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
